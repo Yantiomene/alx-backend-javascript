5e0c2d0afed4449dab095954101c7105c8562639
Yaninthé Tiomene <yan2016tiomene@gmail.com>
2023-11-29 22:34:56 +0000

    [Fix] removing the promise statement and added a try catch statement

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
 
-  return Promise.allSettled([photo, user])
-    .then((values) => {
-      if (values[0].status !== 'fulfilled' || values[1].status !== 'fulfilled') {
-        return { photo: null, user: null };
-      }
-      return { photo: values[0].value, user: values[1].value };
-    });
+    return { photo, user };
+  } catch (e) {
+    return { photo: null, user: null };
+  }
 }
