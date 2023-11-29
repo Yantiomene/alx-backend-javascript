7057f65e1b18bed93faabca0f7d9d92f4430b432
Yaninthé Tiomene <yan2016tiomene@gmail.com>
2023-11-29 21:33:41 +0000

    [feature] Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo])
+    .then((values) => {
+      const result = [];
+      values.forEach((value) => {
+        if (value.status === 'fulfilled') {
+          result.push({ status: value.status, value: value.value });
+        } else {
+          result.push({ status: value.status, value: `${value.reason}` });
+        }
+      });
+      return result;
+    });
+}
