6e8a55c613102772adcfbd2a8fc60eb744936ac2
Yaninthé Tiomene <yan2016tiomene@gmail.com>
2023-11-30 20:43:01 +0000

    [Fix] Fizing typos in error message

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,7 +2,7 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') throw new TypeError('Name must be a string');
     if (!Number.isInteger(length)) throw new TypeError('Length must be a number');
-    if (!Array.isArray(students)) throw new TypeError('Students must be an array');
+    if (!Array.isArray(students)) throw new TypeError('Students type must be an Array');
     this._name = name;
     this._length = length;
     this._students = students;
@@ -31,7 +31,7 @@ export default class HolbertonCourse {
   }
 
   set students(val) {
-    if (Array.isArray(val)) throw new TypeError('Students must be an array');
+    if (!Array.isArray(val)) throw new TypeError('Students type must be an Array');
     this._students = val;
   }
 }
