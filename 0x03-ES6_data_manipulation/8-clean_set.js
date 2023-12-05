e8e87dbd02a813d87a927c12141a775f2d3e864f
Yaninthé Tiomene <yan2016tiomene@gmail.com>
2023-12-05 13:58:42 +0000

    [Fix] fixing the case of an empty string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,8 +2,8 @@ export default function cleanSet(set, startString) {
   let newString = '';
   if (startString === undefined || startString.length === 0) return '';
   newString = [...set]
-    .filter((elt) => elt.startsWith(startString))
-    .map((elt) => elt.replace(startString, ''))
+    .filter((elt) => (elt === undefined ? '' : elt.startsWith(startString)))
+    .map((elt) => (elt === undefined ? '' : elt.replace(startString, '')))
     .join('-');
   return newString;
 }
