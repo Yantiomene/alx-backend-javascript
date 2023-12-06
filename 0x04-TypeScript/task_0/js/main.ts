97904e1fbd3f968d916b15496e934f5507f5722f
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2023-12-06 19:18:19 +0100

    [Fix] Fixing an error

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,35 +1,38 @@
 
 interface Student {
-	  firstName: string;
-	  lastName: string;
-	  age: number;
-	  location: string;
-	  }
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+}
 
 const studentA: Student = {
-      firstName: "Yan",
-      lastName: "Tiomene",
-      age: 20,
-      location: "Yaounde",
-      }
+    firstName: "Yan",
+    lastName: "Tiomene",
+    age: 20,
+    location: "Yaounde",
+}
 
 const studentB: Student = {
-      firstName: "Yaninthe",
-      lastname: "Douanla",
-      age: 30,
-      location: "Douala",
-      }
+    firstName: "Yaninthe",
+    lastName: "Douanla",
+    age: 30,
+    location: "Douala",
+}
 
 const studentsList = [studentA, studentB];
 
-const table = document.getElementById('studentTable') as HTMLTableElement;
+document.addEventListener('DOMContentLoaded', () => {
 
-studentsList.forEach((student) => {
-			       const row = table.inserRow();
-			       const firstNameCell = row.insertCell(0);
-			       const locationCell = row.insertCell(1);
+	const table = document.getElementById('studentTable') as HTMLTableElement;
 
-			       firstNameCell.innerHTML = student.firstName;
-			       locationCell.innerHTML = student.location;
-			       })
+	studentsList.forEach((student) => {
+		const row = table.insertRow();
+		const firstNameCell = row.insertCell(0);
+		const locationCell = row.insertCell(1);
+
+		firstNameCell.innerHTML = student.firstName;
+		locationCell.innerHTML = student.location;
+	})
+})
 
