d61c547f92bb931d9f32c32ffd431e24eadc37a6
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-30 21:30:52 +0100

    [Feat] Adding a script to read a file sync with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,38 @@
+const fs = require('node:fs');
+
+function countStudents (filePath) {
+  let numStud = 0;
+  const students = {};
+  const fields = {};
+
+  try {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    const lines = data.toString().split('\n');
+    for (let i = 0; i < lines.length; i += 1) {
+      if (lines[i]) {
+        numStud += 1;
+        const row = lines[i].toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(students, row[3])) {
+          students[row[3]].push(row[0]);
+        } else {
+          students[row[3]] = [row[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, row[3])) {
+          fields[row[3]] += 1;
+        } else {
+          fields[row[3]] = 1;
+        }
+      }
+    }
+    console.log(`Number of students: ${numStud - 1}`);
+    for (const [k, v] of Object.entries(fields)) {
+      if (k !== 'field') {
+        console.log(`Number of students in ${k}: ${v}. List: ${students[k].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw Error('cannot load the database');
+  }
+}
+
+module.exports = countStudents;
