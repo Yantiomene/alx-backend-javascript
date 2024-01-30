6ee1b74fe3a3b1bcbcf67ccc4dfdfe977b29c7dc
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-30 22:19:56 +0100

    [Feat] Adding a script to read a file async

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,42 @@
+const { readFile } = require('fs');
+
+function countStudents (filePath) {
+  let numStud = 0;
+  const students = {};
+  const fields = {};
+
+  return new Promise((resolve, reject) => {
+    readFile(filePath, (error, data) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            numStud += 1;
+            const row = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, row[3])) {
+              students[row[3]].push(row[0]);
+            } else {
+              students[row[3]] = [row[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, row[3])) {
+              fields[row[3]] += 1;
+            } else {
+              fields[row[3]] = 1;
+            }
+          }
+        }
+        console.log(`Number of students: ${numStud - 1}`);
+        for (const [k, v] of Object.entries(fields)) {
+          if (k !== 'field') {
+            console.log(`Number of students in ${k}: ${v}. List: ${students[k].join(', ')}`);
+          }
+        }
+        resolve(data);
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
